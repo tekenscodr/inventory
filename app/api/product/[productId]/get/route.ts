b72c0695af18143930dacb763b8677f69285490e
tekenscodr <ensleybdarku@gmail.com>
2024-08-14 12:58:18 +0100

    another-correction-to-init-

--- app/api/product/[productId]/get/route.ts
+++ app/api/product/[productId]/get/route.ts
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/prismadb"
 
-export async function GET(request: Request, context: any) {
+export async function GET(context: any) {
     const { params } = context;
     const productId = await params.productId
     try {
-        return prisma.products.findFirst({
+        const product = await prisma.products.findFirst({
             where: {
                 productID: productId
             }
         })
+        return NextResponse.json(product)
     } catch (error) {
         return NextResponse.json(error)
     }
